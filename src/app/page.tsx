3b0a032ab02be97ce702a60e0a2ba1fba2fe4dd5
Lautaro Camejo <lautarocamejo@gmail.com>
2024-10-13 02:49:38 -0300

    dialog to win

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,24 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default function HomePage() {
-  ///CONTADORES
+  ///COUNTERS
   const [counter, setCounter] = useState(0);
   const [secondCounter, setSecondCounter] = useState(0);
 
-  ///ESTADOS DE BUENAS, MALAS Y GANADOR
+  ///STATS GOODS, BADS, AND WIN
   const [ourGoods, setOurGoods] = useState<boolean>(false);
   const [theyGoods, setTheyGoods] = useState<boolean>(false);
   const [win, setWin] = useState<boolean | null>(null);
@@ -52,11 +63,7 @@ export default function HomePage() {
   };
 
   const isWin = () => {
-    counter > 15 && ourGoods
-      ? (setWin(true), alert("Gano el jugador 1"))
-      : secondCounter > 15 && theyGoods
-        ? (setWin(true), alert("Gano el jugador 2"))
-        : "";
+    counter > 14 && ourGoods ? setWin(true) : secondCounter > 14 && theyGoods ? setWin(true) : "";
   };
 
   const resetOurStats = () => {
@@ -76,6 +83,21 @@ export default function HomePage() {
     setWin(false);
   };
 
+  function showWinDialog() {
+    return (
+      <AlertDialog open>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{counter === 15 ? "Our" : "They"} Wins </AlertDialogTitle>
+            <AlertDialogDescription>To reset the party, press continue! </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction onClick={resetGame}>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
+  }
   useEffect(() => {
     isGoods();
     isWin();
@@ -83,57 +105,60 @@ export default function HomePage() {
 
   useEffect(() => {
     if (win) {
-      resetGame();
     }
   }, [win]);
 
   return (
-    <div className="flex items-center justify-evenly">
-      <Card className="w-60 border-gray-500 transition-all ease-in  hover:cursor-pointer hover:border-white hover:delay-100">
-        <CardHeader>
-          <CardTitle className="flex justify-center">Nosotros</CardTitle>
-          <CardDescription className="flex justify-center">
-            {ourGoods ? "Buenas" : "Malas"}
-          </CardDescription>
-          <CardDescription>Puntaje: {counter}</CardDescription>
-        </CardHeader>
-        <CardContent className="flex justify-between">
-          <Button className="bg-white text-black" variant="outline" onClick={handleOurRes}>
-            Restar
-          </Button>
-          <Button className="bg-white text-black" variant="outline" onClick={handleOurSum}>
-            Sumar
-          </Button>
-        </CardContent>
-        <CardFooter className="flex justify-center">
-          <Button className="bg-white text-black" variant="outline" onClick={resetOurStats}>
-            Reset
-          </Button>
-        </CardFooter>
-      </Card>
+    <div>
+      <div className="flex items-center justify-evenly">
+        <Card className="w-60 border-gray-500 transition-all ease-in  hover:cursor-pointer hover:border-white hover:delay-100">
+          <CardHeader>
+            <CardTitle className="flex justify-center">Nosotros</CardTitle>
+            <CardDescription className="flex justify-center">
+              {ourGoods ? "Goods" : "Bads"}
+            </CardDescription>
+            <CardDescription>Score: {counter}</CardDescription>
+          </CardHeader>
+          <CardContent className="flex justify-between">
+            <Button className="bg-white text-black" variant="outline" onClick={handleOurRes}>
+              Restar
+            </Button>
+            <Button className="bg-white text-black" variant="outline" onClick={handleOurSum}>
+              Sumar
+            </Button>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button className="bg-white text-black" variant="outline" onClick={resetOurStats}>
+              Reset
+            </Button>
+          </CardFooter>
+        </Card>
 
-      <Card className="w-60 border-gray-500 transition-all ease-in  hover:cursor-pointer hover:border-white hover:delay-100 ">
-        <CardHeader>
-          <CardTitle className="flex justify-center">Ellos</CardTitle>
-          <CardDescription className="flex justify-center">
-            {theyGoods ? "Buenas" : "Malas"}
-          </CardDescription>
-          <CardDescription>Puntaje: {secondCounter}</CardDescription>
-        </CardHeader>
-        <CardContent className="flex justify-between">
-          <Button className="bg-white text-black" variant="outline" onClick={handleTheyRes}>
-            Restar
-          </Button>
-          <Button className="bg-white text-black" variant="outline" onClick={handleTheySum}>
-            Sumar
-          </Button>
-        </CardContent>
-        <CardFooter className="flex justify-center">
-          <Button className="bg-white text-black" variant="outline" onClick={resetTheyStats}>
-            Reset
-          </Button>
-        </CardFooter>
-      </Card>
+        <Card className="w-60 border-gray-500 transition-all ease-in  hover:cursor-pointer hover:border-white hover:delay-100 ">
+          <CardHeader>
+            <CardTitle className="flex justify-center">Ellos</CardTitle>
+            <CardDescription className="flex justify-center">
+              {theyGoods ? "Goods" : "Bads"}
+            </CardDescription>
+            <CardDescription>Score: {secondCounter}</CardDescription>
+          </CardHeader>
+          <CardContent className="flex justify-between">
+            <Button className="bg-white text-black" variant="outline" onClick={handleTheyRes}>
+              Restar
+            </Button>
+            <Button className="bg-white text-black" variant="outline" onClick={handleTheySum}>
+              Sumar
+            </Button>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button className="bg-white text-black" variant="outline" onClick={resetTheyStats}>
+              Reset
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+
+      <div className="flex justify-center">{win ? showWinDialog() : ""}</div>
     </div>
   );
 }

f48189a1a80356ebb6d30ec268fbfca2d7875e53
Lautaro Camejo <lautarocamejo@gmail.com>
2024-10-13 02:02:06 -0300

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,6 +28,9 @@ export default function HomePage() {
   const handleOurRes = () => {
     if (!(counter === 0)) {
       setCounter(counter - 1);
+    } else if (ourGoods) {
+      setOurGoods(false);
+      setCounter(15);
     }
   };
   const handleTheySum = () => {
@@ -37,12 +40,15 @@ export default function HomePage() {
   const handleTheyRes = () => {
     if (!(secondCounter === 0)) {
       setSecondCounter(secondCounter - 1);
+    } else if (theyGoods) {
+      setTheyGoods(false);
+      setSecondCounter(15);
     }
   };
 
   const isGoods = () => {
-    counter > 15 ? (setOurGoods(true), resetCounter()) : "";
-    secondCounter > 15 ? (setTheyGoods(true), resetSecondCounter()) : "";
+    counter > 15 ? (setOurGoods(true), setCounter(0)) : "";
+    secondCounter > 15 ? (setTheyGoods(true), setSecondCounter(0)) : "";
   };
 
   const isWin = () => {
@@ -53,12 +59,15 @@ export default function HomePage() {
         : "";
   };
 
-  const resetCounter = () => {
+  const resetOurStats = () => {
     setCounter(0);
+    setOurGoods(false);
   };
-  const resetSecondCounter = () => {
+  const resetTheyStats = () => {
     setSecondCounter(0);
+    setTheyGoods(false);
   };
+
   const resetGame = () => {
     setCounter(0);
     setSecondCounter(0);
@@ -80,7 +89,7 @@ export default function HomePage() {
 
   return (
     <div className="flex items-center justify-evenly">
-      <Card className="w-60 hover:cursor-pointer hover:border-white">
+      <Card className="w-60 border-gray-500 transition-all ease-in  hover:cursor-pointer hover:border-white hover:delay-100">
         <CardHeader>
           <CardTitle className="flex justify-center">Nosotros</CardTitle>
           <CardDescription className="flex justify-center">
@@ -89,15 +98,21 @@ export default function HomePage() {
           <CardDescription>Puntaje: {counter}</CardDescription>
         </CardHeader>
         <CardContent className="flex justify-between">
-          <Button onClick={handleOurSum}>Sumar</Button>
-          <Button onClick={handleOurRes}>Restar</Button>
+          <Button className="bg-white text-black" variant="outline" onClick={handleOurRes}>
+            Restar
+          </Button>
+          <Button className="bg-white text-black" variant="outline" onClick={handleOurSum}>
+            Sumar
+          </Button>
         </CardContent>
         <CardFooter className="flex justify-center">
-          <Button onClick={resetCounter}>Reset</Button>
+          <Button className="bg-white text-black" variant="outline" onClick={resetOurStats}>
+            Reset
+          </Button>
         </CardFooter>
       </Card>
 
-      <Card className="w-60 hover:cursor-pointer hover:border-white ">
+      <Card className="w-60 border-gray-500 transition-all ease-in  hover:cursor-pointer hover:border-white hover:delay-100 ">
         <CardHeader>
           <CardTitle className="flex justify-center">Ellos</CardTitle>
           <CardDescription className="flex justify-center">
@@ -106,11 +121,17 @@ export default function HomePage() {
           <CardDescription>Puntaje: {secondCounter}</CardDescription>
         </CardHeader>
         <CardContent className="flex justify-between">
-          <Button onClick={handleTheySum}>Sumar</Button>
-          <Button onClick={handleTheyRes}>Restar</Button>
+          <Button className="bg-white text-black" variant="outline" onClick={handleTheyRes}>
+            Restar
+          </Button>
+          <Button className="bg-white text-black" variant="outline" onClick={handleTheySum}>
+            Sumar
+          </Button>
         </CardContent>
         <CardFooter className="flex justify-center">
-          <Button onClick={resetSecondCounter}>Reset</Button>
+          <Button className="bg-white text-black" variant="outline" onClick={resetTheyStats}>
+            Reset
+          </Button>
         </CardFooter>
       </Card>
     </div>
